30ed47a9b8659297b0d89608f3f5e648303eeb88
Gonçalo Pestana <g6pestana@gmail.com>
2024-07-24 18:03:14 +0100

    docs

--- relay/temp-pallets/pallet-staking-fixer/src/lib.rs
+++ relay/temp-pallets/pallet-staking-fixer/src/lib.rs
@@ -13,9 +13,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! # Temporary pallet to fix staking ledgers.
+//! # Temporary pallet to fix corrupted staking ledgers.
 //!
-//! > TODO
+//! This single-purpose pallet exposes an extrinsic which works as a proxy to
+//! `Staking::restore_ledger`. There are three differences between the extrinsic exposed by this
+//! pallet and the exposed by the Staking pallet, namely:
+//!
+//! - Access control: any signed account may call `Call::restore_ledger`. The call to
+//! `Staking::restore_ledger` from this pallet will be performed with Origin::Root. It is safe to
+//! expose the call to the sign origin since *only* corrupted ledgers can be restored. If the stash
+//! is not corrupted, the call is a no-op (modulo paid fees).
+//! - Default inputs: `Call::restore_ledger` exposes less inputs than the proxied
+//! `Staking::restore_ledger` call, providing sensible defaults that are enough to restore all
+//! corrupted ledgers.
+//! - Extra safety check: if the resulting restored ledger from `Staking::restore_ledger` has a
+//! larger total staked than the stash's free balance, the ledger is unstaked.
+//!
+//! Note: this pallet is temporary and can be removed when all the ledgers in Polkadot and Kusama
+//! are restored.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -36,9 +51,6 @@ pub mod pallet {
 	pub trait Config: frame_system::Config + pallet_staking::Config {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-
-		/// Weight information for extrinsics in this pallet.
-		type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::pallet]
@@ -56,13 +68,21 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// TODO
+		/// Restores the state of a staking ledger that is in an inconsistent state.
+		///
+        /// This call functions as a proxy to `Staking::restore_ledger`, but exposes it to signed
+        /// origins. In addition, this call checks if, after the ledger has been successfully
+        /// restored, the ledger locks are higher or equal than the stash's free balance. If not,
+        /// it forces the unstake of the ledger.
+        ///
+		/// Safety note: Only ledgers associated `stash` that are corrupted will be mutated. Thus it
+		/// is safe to expose `Staking::restore_ledger` through the signed origin.
 		#[pallet::call_index(0)]
 		#[pallet::weight(
-            <<T as pallet::Config>::WeightInfo>::restore_ledger() +
-            <<T as pallet::Config>::WeightInfo>::force_unstake(maybe_slashing_spans.unwrap_or_default())
+            <T::WeightInfo>::restore_ledger() +
+            <T::WeightInfo>::force_unstake(maybe_slashing_spans.unwrap_or_default())
         )]
-		pub fn restore_ledger_temp(
+		pub fn restore_ledger(
 			origin: OriginFor<T>,
 			stash: T::AccountId,
 			maybe_slashing_spans: Option<u32>,
@@ -93,12 +113,11 @@ pub mod pallet {
 
 				Self::deposit_event(Event::<T>::RestoredUnstaked { stash });
 
-				<<T as pallet::Config>::WeightInfo>::restore_ledger() +
-					<<T as pallet::Config>::WeightInfo>::force_unstake(slashing_spans)
+				<T::WeightInfo>::restore_ledger() + <T::WeightInfo>::force_unstake(slashing_spans)
 			} else {
 				Self::deposit_event(Event::<T>::Restored { stash });
 
-				<<T as pallet::Config>::WeightInfo>::restore_ledger()
+				<T::WeightInfo>::restore_ledger()
 			};
 
 			Ok(Some(weight).into())
